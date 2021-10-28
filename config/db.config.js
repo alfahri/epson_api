0dbd848af8c0beadcead6019adcbf57ba35b4eb6
AL <m.alfahri98@gmail.com>
2021-10-29 02:19:33 +0700

    upload

--- config/db.config.js
+++ config/db.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  HOST: "192.168.10.31",
-  USER: "alfa",
-  PASSWORD: "db_devepson",
-  DB: "db_devepson",
+  HOST: "192.168.10.39",
+  USER: "echonano",
+  PASSWORD: "tePhi2i=8UHlPe6IcHiy",
+  DB: "db_staging_epson",
   dialect: "mysql",
   pool: {
     max: 5,
