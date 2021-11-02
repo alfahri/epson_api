59f6aeaa2e5b668bd26e17e7ef0535a4c94132de
AL <m.alfahri98@gmail.com>
2021-11-02 22:09:54 +0700

    no message

--- config/db.config.js
+++ config/db.config.js
@@ -2,7 +2,7 @@ module.exports = {
   HOST: "192.168.10.39",
   USER: "echonano",
   PASSWORD: "tePhi2i=8UHlPe6IcHiy",
-  DB: "db_staging_epson",
+  DB: "live_epson",
   dialect: "mysql",
   pool: {
     max: 5,
