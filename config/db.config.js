7d916e9002698bf63c0b6e3dbb33f2fe946f29ca
Server <m.alfahri98@gmail.com>
2021-11-11 01:17:18 +0800

    gas

--- config/db.config.js
+++ config/db.config.js
@@ -2,7 +2,7 @@ module.exports = {
   HOST: "192.168.10.39",
   USER: "echonano",
   PASSWORD: "tePhi2i=8UHlPe6IcHiy",
-  DB: "live_epson",
+  DB: "db_staging_epson",
   dialect: "mysql",
   pool: {
     max: 5,
